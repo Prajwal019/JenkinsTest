77d47bac5e2bd990c8ba8a440611b6dfb60ab431
PrajwalMoorthy <prajwal.pr@aspireapp.com>
2024-05-21 14:58:26 +0530

    changes

--- src/test/java/TestSample.java
+++ src/test/java/TestSample.java
@@ -62,8 +62,6 @@ public class TestSample
         for (int i = 0; i < input.length(); i = i + 2)
         {
             char ch = input.charAt(i);
-
-            // Get the count (the next character in the string)
             // Assuming single digit count (0-9), convert char to int
             int count = input.charAt(i + 1) - '0';
             for (int j = 0; j < count; j++)
